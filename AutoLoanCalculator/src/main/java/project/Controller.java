ec922796fd95f68f0bb33f0b366ff23793638dd0
Arnav Madavaram <madaa01@pfw.edu>
2025-02-23 12:01:32 -0500

    Add files via upload

---
+++ AutoLoanCalculator/src/main/java/project/Controller.java
@@ -0,0 +1,31 @@
+package project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class Controller {
+
+    @FXML
+    private RadioButton RanaRad;
+
+    @FXML
+    private Label printLbl;
+
+    @FXML
+    private RadioButton ArnavRad;
+
+    @FXML
+    private RadioButton RaviRad;
+
+    @FXML
+    private RadioButton RohanRad;
+
+    @FXML
+    private RadioButton ShanmukRad;
+
+    @FXML
+    private Button activityBtn;
+
+}
